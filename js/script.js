54abf61a97c5e42d5f951bb33a25b8846598c290
Jose Navarro <jfnr398@gmail.co>
2024-09-11 15:50:49 -0500

    Index.html HEADER José Navarro

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let contador = 1;
+    setInterval(() => {
+        contador++;
+
+        const fondo = new Image();
+        fondo.src = `../img/banner${contador}.jpg`; //Precarga imagen
+
+        fondo.addEventListener("load", function(){
+            document.querySelector(".encabezado__banner").style.backgroundImage = `url('${fondo.src}')`;
+        });
+
+        if(contador == 4){
+            contador = 0;
+        }
+        
+    }, 10000);
+});
